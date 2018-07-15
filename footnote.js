19d6ed3258c815cf7f8ad0a7fa00e14802100436
れもん <lemoncmd@gmail.com>
2018-07-15 19:23:23 +0900

    2nd day

---
+++ footnote.js
@@ -0,0 +1,23 @@
+var footnoting = 0;
+
+function footnote(element, num){
+	num++;
+	console.log(element)
+	element.innerText = `[${num}]`;
+	element.href=`#ref${num}`;
+	element.id=`ft${num}`;
+	element.addEventListener("click", function(e){
+		var element = document.getElementById(`bold${footnoting}`);
+		if(element)element.className = "normal";
+		footnoting = num;
+		element = document.getElementById(`bold${footnoting}`);
+		if(element)element.className = "bold";
+	});
+	document.getElementById("footer").innerHTML += `<br><a id="ref${num}"><a href="#ft${num}"><b id="bold${num}" class="normal">[${num}]</b></a> ${element.title}</a>`;
+}
+
+addEventListener("load", function(){
+	var elements = document.querySelectorAll("a.ref[title]");
+	console.log(1);
+	[].slice.call(elements).map(footnote);
+});
